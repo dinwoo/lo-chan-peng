f5478aab7f8a4328386bc4918df1284de19209c6
Daniel Wu <daniel.wu@language-center.com.tw>
2021-09-07 14:33:34 +0800

    feat: 課程購買連結串接

--- src/views/CourseInfo.vue
+++ src/views/CourseInfo.vue
@@ -5,7 +5,8 @@ article.course-info(v-if="!isLoading")
       img(src="@/assets/images/course-icon.png")
   section.main
     CardInfo(:cardData="course.detail")
-    Button(title='課 程')
+    //- Button(title='課 程')
+    a.buy-btn(:href="course.detail.buyScript") 課 程
   section.related
     .title 相關文章
     RelatedList(:relatedListData="course.detail.otherCourses" routeName="CourseInfo")
@@ -14,91 +15,91 @@ article.course-info(v-if="!isLoading")
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
-import CardInfo from "@/components/CardInfo.vue";
-import RelatedList from "@/components/RelatedList.vue";
-import Button from "@/components/Button.vue";
-import { gsap } from "gsap";
+import { mapState, mapActions } from "vuex"
+import CardInfo from "@/components/CardInfo.vue"
+import RelatedList from "@/components/RelatedList.vue"
+import Button from "@/components/Button.vue"
+import { gsap } from "gsap"
 
 export default {
   name: "CourseInfo",
   components: {
     CardInfo,
     RelatedList,
-    Button,
+    Button
   },
   data() {
     return {
-      sceneArr: [],
-    };
+      sceneArr: []
+    }
   },
   computed: {
-    ...mapState(["isLoading", "course"]),
+    ...mapState(["isLoading", "course"])
   },
   beforeDestroy() {
     this.sceneArr.map((scene) => {
-      this.$scrollmagic.removeScene(scene);
-    });
+      this.$scrollmagic.removeScene(scene)
+    })
   },
   mounted() {
-    this.$nextTick(() => {});
+    this.$nextTick(() => {})
   },
   created() {
     this.getCourseDetail({
       id: parseInt(this.$route.params.id),
-      channel: this.lang,
+      channel: this.lang
     })
       .then(() => {
-        console.log("success");
-        this.setInitial();
-        this.setAnimate();
+        console.log("success")
+        this.setInitial()
+        this.setAnimate()
       })
       .catch(() => {
-        console.log("fail");
-      });
+        console.log("fail")
+      })
   },
   methods: {
     ...mapActions(["getCourseDetail"]),
     setInitial() {
       gsap.set("section.banner", {
-        opacity: 0,
-      });
+        opacity: 0
+      })
       gsap.set("section.related", {
         y: 50,
-        opacity: 0,
-      });
+        opacity: 0
+      })
     },
     setAnimate() {
       this.sceneArr[0] = this.$scrollmagic
         .scene({
           triggerElement: "section.banner",
-          reverse: false,
+          reverse: false
         })
         .setTween("section.banner", 1, {
-          opacity: 1,
-        });
+          opacity: 1
+        })
       // .addIndicators({ name: "banner" })
 
       this.sceneArr[1] = this.$scrollmagic
         .scene({
           triggerElement: "section.related",
           reverse: false,
-          triggerHook: 0.8,
+          triggerHook: 0.8
         })
         .setTween("section.related", 1, {
           y: 0,
-          opacity: 1,
-        });
+          opacity: 1
+        })
       // .addIndicators({ name: "related" })
 
       this.sceneArr.forEach((scene) => {
-        console.log(scene);
-        this.$scrollmagic.addScene(scene);
-      });
-    },
+        console.log(scene)
+        this.$scrollmagic.addScene(scene)
+      })
+    }
   },
-  watch: {},
-};
+  watch: {}
+}
 </script>
 
 <style lang="sass" scoped>
@@ -116,6 +117,18 @@ article.course-info
     padding: 0 15px
     margin: auto
     box-sizing: border-box
+    .buy-btn
+      display: block
+      width: 100%
+      max-width: 300px
+      margin: 3rem auto
+      font-size: 1.4rem
+      line-height: 4rem
+      border-radius: 2rem
+      color: $gray-001
+      background-color: $gray-005
+      text-align: center
+      cursor: pointer
   section.related
     padding-top: 100px
     .title
