5c846c4c024df83dc1297e4fd2dd0621318ef94f
dinwoo <danielforboy@gmail.com>
2023-04-11 13:22:31 +0800

    feat: 調整字體顏色及news排版

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -5,9 +5,13 @@
     :to="{name:routeName,params:{id:card.id}}"
   )
     .date {{card.dateTime}}
-    .card-pic(:style="`background-image: url('${card.img}')`")
-    .card-title {{card.title}}
-    .card-description {{card.description}}
+    .card-content
+      .card-pic
+        //- (:style="`background-image: url('${card.img}')`")
+        img(:src="card.img")
+      .card-info
+        .card-title {{card.title}}
+        .card-description {{card.description}}
 
 </template>
 
@@ -22,16 +26,16 @@ export default {
   components: {},
   props: {
     cardData: Array,
-    routeName: String,
+    routeName: String
   },
   data() {
     return {
-      sceneArr: [],
+      sceneArr: []
     };
   },
   watch: {},
   beforeDestroy() {
-    this.sceneArr.map((scene) => {
+    this.sceneArr.map(scene => {
       this.$scrollmagic.removeScene(scene);
     });
   },
@@ -46,7 +50,7 @@ export default {
     setInitial() {
       gsap.set(".card-list-wrapper .card-item", {
         y: 50,
-        opacity: 0,
+        opacity: 0
       });
     },
     setAnimate() {
@@ -54,7 +58,7 @@ export default {
         .scene({
           triggerElement: ".card-list-wrapper .card-item",
           reverse: false,
-          triggerHook: 1,
+          triggerHook: 1
         })
         .on("enter", function() {
           TweenMax.staggerTo(
@@ -62,18 +66,18 @@ export default {
             1,
             {
               y: 0,
-              opacity: 1,
+              opacity: 1
             },
             0.2
           );
         });
       // .addIndicators({ name: "card-list" });
 
-      this.sceneArr.forEach((scene) => {
+      this.sceneArr.forEach(scene => {
         this.$scrollmagic.addScene(scene);
       });
-    },
-  },
+    }
+  }
 };
 </script>
 
@@ -85,52 +89,73 @@ export default {
   justify-content: space-between
   flex-wrap: wrap
   .card-item
-    width: 46%
+    width: 100%
     margin-bottom: 2rem
     display: block
     .date
       margin-bottom: 1rem
       font-size: 1.4rem
       color: $gray-004
-    .card-pic
-      // background-image: url('../assets/images/news-example.jpg')
-      width: 100%
-      padding-bottom: 63.701923%
-      border-radius: 10px
-      background-size: cover
-      background-position: center center
-      background-repeat: no-repeat
-      position: relative
-      +before
-        width: 100%
-        height: 100%
+    .card-content
+      font-size: 0
+      .card-pic
+        display: inline-block
+        vertical-align: top
+        // background-image: url('../assets/images/news-example.jpg')
+        width: 40%
+        max-width: 575px
         border-radius: 10px
-        background-color: rgba(#000,.2)
-        transition: .3s
-        position: absolute
-        top: 0
-        left: 0
-      +hover
-        &:before
-          background-color: rgba(#000,0)
-    .card-title
-      margin: 1.2rem 0
-      font-size: 1.4rem
-      color: $gray-004
-    .card-description
-      font-size: 1rem
-      line-height: 1.5
-      color: $gray-005
-      display: -webkit-box
-      -webkit-line-clamp: 2
-      -webkit-box-orient: vertical
-      white-space: normal
-      overflow: hidden
+        overflow: hidden
+        // padding-bottom: 63.701923%
+        // background-size: cover
+        // background-position: center center
+        // background-repeat: no-repeat
+        position: relative
+        +before
+          width: 100%
+          height: 100%
+          border-radius: 10px
+          background-color: rgba(#000,.2)
+          transition: .3s
+          position: absolute
+          top: 0
+          left: 0
+        +hover
+          &:before
+            background-color: rgba(#000,0)
+      .card-info
+        display: inline-block
+        vertical-align: top
+        width: 60%
+        padding-left: 1rem
+        box-sizing: border-box
+        .card-title
+          margin-bottom: 1.2rem
+          font-size: 1.4rem
+          color: $gray-004
+        .card-description
+          font-size: 1rem
+          line-height: 1.5
+          color: $gray-005
+          display: -webkit-box
+          -webkit-line-clamp: 2
+          -webkit-box-orient: vertical
+          white-space: normal
+          overflow: hidden
   +rwd(768px)
     .card-item
       width: 100%
       .date
-      .card-pic
-      .card-title
-      .card-description
+      .card-content
+        .card-pic
+          display: block
+          width: 100%
+          max-width: none
+        .card-info
+          display: block
+          width: 100%
+          padding-left: 0
+          .card-title
+            margin: 1.2rem 0
+          .card-description
 </style>
